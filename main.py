bb4d570086ffc3817cddfcd691afcc14384d3abb
Hyoje Lee <hyoje42@snu.ac.kr>
2020-12-23 14:47:44 +0900

    change tensorboard package

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ import argparse
 import numpy as np
 import torch
 from torch.optim.lr_scheduler import StepLR
-
-import tensorboard_logger as tb_logger
+from torch.utils.tensorboard import SummaryWriter
 
 # custom packages
 from dataset import dataset_cifar
@@ -67,7 +66,7 @@ def parser_arg():
 
     ## debug
     # args, _ = parser.parse_known_args('-g 0 --exp_name debug --seed 777 \
-    #                                    --backbone resnet18 --method SelfKD_KL_Delay \
+    #                                    --backbone resnet18 --method BaseMethod \
     #                                    --batch_size 128'.split())
                                        
     ## real
@@ -75,87 +74,90 @@ def parser_arg():
 
     return add_args(args)
 
-# set environment variables: gpu, num_thread
-args = parser_arg()
-os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu)
-os.environ['OMP_NUM_THREADS'] = '1'
-os.environ['MKL_NUM_THREADS'] = '1'
-# torch.set_num_threads(1)
+if __name__ == "__main__":
+        
+    # set environment variables: gpu, num_thread
+    args = parser_arg()
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu)
+    os.environ['OMP_NUM_THREADS'] = '1'
+    os.environ['MKL_NUM_THREADS'] = '1'
+    # torch.set_num_threads(1)
 
-# RANDOM SEED
-def seed(seed_num):
-    random.seed(seed_num)
-    np.random.seed(seed_num)
-    torch.manual_seed(seed_num)
+    # RANDOM SEED
+    def seed(seed_num):
+        random.seed(seed_num)
+        np.random.seed(seed_num)
+        torch.manual_seed(seed_num)
+        if torch.cuda.is_available():
+            torch.cuda.manual_seed(seed_num)
+            torch.cuda.manual_seed_all(seed_num) # if use multi-GPU
+        # It could be slow
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+    if args.seed:    
+        seed(args.seed)
+    ### Step 1: init dataloader
+    train_dataset, test_dataset = dataset_cifar('cifar100', aug=args.aug)
+    trainloader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size,
+                                                shuffle=True, num_workers=1)
+    testloader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size*2,
+                                                shuffle=False, num_workers=1)
+    ### Step 2: init neural networks
+    print("init neural networks")
+    ## construct the model
+    backbone = resnet.__dict__[args.backbone](num_classes=100)
+    if 'Base' in args.method or 'Self' in args.method:
+        model = models.__dict__[args.method](args, backbone)
+    elif args.method in ['AFD', 'DML']:
+        backbone2 = resnet.__dict__[args.backbone](num_classes=100)
+        model = models.__dict__[args.method](args, backbone, backbone2)
+    else:
+        print(f'{args.method} is not available')
+        raise NotImplementedError()
     if torch.cuda.is_available():
-        torch.cuda.manual_seed(seed_num)
-        torch.cuda.manual_seed_all(seed_num) # if use multi-GPU
-    # It could be slow
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
-if args.seed:    
-    seed(args.seed)
-### Step 1: init dataloader
-train_dataset, test_dataset = dataset_cifar('cifar100', aug=args.aug)
-trainloader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size,
-                                            shuffle=True, num_workers=1)
-testloader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size*2,
-                                            shuffle=False, num_workers=1)
-### Step 2: init neural networks
-print("init neural networks")
-## construct the model
-backbone = resnet.__dict__[args.backbone](num_classes=100)
-if 'Base' in args.method or 'Self' in args.method:
-    model = models.__dict__[args.method](args, backbone)
-elif args.method in ['AFD', 'DML']:
-    backbone2 = resnet.__dict__[args.backbone](num_classes=100)
-    model = models.__dict__[args.method](args, backbone, backbone2)
-else:
-    print(f'{args.method} is not available')
-    raise NotImplementedError()
-if torch.cuda.is_available():
-    model.cuda()
+        model.cuda()
 
-# calculate the number of parameters
-cal_num_parameters(model.parameters(), file=args.logfile)
+    # calculate the number of parameters
+    cal_num_parameters(model.parameters(), file=args.logfile)
 
-############### Training ###############
-## log
-log_list = [meter for meter in model.set_log(0, 0)[0].values() if not meter.name == 'Data']
-log_list.append(AverageMeter('Test_Acc', ':.4f'))
-progress = ProgressMeter(args.epochs, meters=log_list, prefix=f'EPOCH')
-logger = tb_logger.Logger(logdir=args.tb_folder)
-end = time.time()
-max_acc = 0.0
-for epoch in range(1, args.epochs+1):
-    ## train
-    loss_list = model.train_loop(trainloader, epoch=epoch)
-    log_list[0].update(time.time() - end)
-    
-    ## eval
-    eval_acc = model.evaluation(testloader)
-    
+    ############### Training ###############
     ## log
-    for i, loss in enumerate(loss_list):
-        log_list[i+1].update(loss.avg)
-        logger.log_value(loss.name, loss.avg, epoch)
-    lr_name = 'lr'
-    for i, opt in enumerate(model.optimizer.optimizers):
-        logger.log_value(lr_name, opt.param_groups[0]['lr'], epoch)
-        lr_name += f'_{i+2}'
-    log_list[-1].update(eval_acc)
-    logger.log_value(log_list[-1].name, eval_acc, epoch)
+    log_list = [meter for meter in model.set_log(0, 0)[0].values() if not meter.name == 'Data']
+    log_list.append(AverageMeter('Test_Acc', ':.4f'))
+    progress = ProgressMeter(args.epochs, meters=log_list, prefix=f'EPOCH')
+    logger = SummaryWriter(log_dir=args.tb_folder)
+    end = time.time()
+    max_acc = 0.0
+    for epoch in range(1, args.epochs+1):
+        ## train
+        loss_list = model.train_loop(trainloader, epoch=epoch)
+        log_list[0].update(time.time() - end)
+        
+        ## eval
+        eval_acc = model.evaluation(testloader)
+        
+        ## log
+        for i, loss in enumerate(loss_list):
+            log_list[i+1].update(loss.avg)
+            logger.add_scalar(loss.name, loss.avg, epoch)
+        lr_name = 'lr'
+        for i, opt in enumerate(model.optimizer.optimizers):
+            logger.add_scalar(lr_name, opt.param_groups[0]['lr'], epoch)
+            lr_name = f'lr_{i+2}'
+        log_list[-1].update(eval_acc)
+        logger.add_scalar(log_list[-1].name, eval_acc, epoch)
 
-    log(progress.display(epoch), args.logfile, consol=False)
+        log(progress.display(epoch), args.logfile, consol=False)
 
-    ## save
-    state = {'args' : args,
-             'epoch' : epoch,
-             'state_dict' : model.state_dict(),}
-            #  'optimizer' : model.optimizer.state_dict()}
+        ## save
+        state = {'args' : args,
+                'epoch' : epoch,
+                'state_dict' : model.state_dict(),}
+                #  'optimizer' : model.optimizer.state_dict()}
 
-    if max_acc < eval_acc:
-        max_acc = eval_acc
-        filename = os.path.join(args.save_folder, 'checkpoint_best.pth.tar')
-        log('#'*20+'Save Best Model'+'#'*20, args.logfile)
-        torch.save(state, filename)
+        if max_acc < eval_acc:
+            max_acc = eval_acc
+            filename = os.path.join(args.save_folder, 'checkpoint_best.pth.tar')
+            log('#'*20+'Save Best Model'+'#'*20, args.logfile)
+            torch.save(state, filename)
+    logger.close()
